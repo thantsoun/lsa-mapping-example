9c4fb8a861cc2193c67cb7eaa86bff1c63e4f7bb
atsounis <athanasios.tsounis@cern.ch>
2021-12-18 18:56:07 +0100

    cleanup

--- src/main/java/cern/lsa/mapping/example/rest/ExampleController.java
+++ src/main/java/cern/lsa/mapping/example/rest/ExampleController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.concurrent.atomic.AtomicReference;
 
 @RestController
 public class ExampleController {
